2b64a264a600da817e15056f0dcf02f4cc292599
Matthew Jeffries <msjeffries21@gmail.com>
2015-08-11 23:19:49 -0400

    post.rb file fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,3 @@
 class Post < ActiveRecord::Base
-	attr_accessible :content, :name, :title
- 
-  validates :name,  :presence => true
-  validates :title, :presence => true,
-                    :length => { :minimum => 5 }
-end
+	
 end
